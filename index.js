7bd906fc670b847fa622455b1b3efa300c9f62e6
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-05 15:44:48 +0900

    Add relationship route

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add Custom Routes
+server.use('/users/:user_id/stocked/:item_id', (req, res, next) => {
+  const userId = req.params['user_id']
+  const itemId = req.params['item_id']
+  const user = router.db.get('users').getById(userId).cloneDeep().value()
+  const userStockedItems = router.db.get('users_stockedItems').cloneDeep().value()
+  const filteredRelations = userStockedItems.filter(item => item.userId === userId)
+  const items = filteredRelations.find(item => item.itemId === itemId)
+  const item = router.db.get('items').getById(itemId)
+  return res.status(200).json(item)
+})
+
+server.use('/users/:user_id/stocked', (req, res, next) => {
+  const userId = req.params['user_id']
+  const user = router.db.get('users').getById(userId).cloneDeep().value()
+  const userStockedItems = router.db.get('users_stockedItems').cloneDeep().value()
+  const filteredRelations = userStockedItems.filter(item => item.userId === userId)
+  const result = []
+  const items = [] 
+  filteredRelations.forEach(relation => {
+    const item = router.db.get('items').getById(relation.itemId).cloneDeep().value()
+    items.push(item)
+  })
+  return res.status(200).json(items)
+})
+
 server.use(router)
 
 server.listen(3000, () => {
