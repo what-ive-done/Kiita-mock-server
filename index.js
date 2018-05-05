4c94591c1ecdc40e2d9df980e1ae2e8d8a35cc68
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-05 12:51:44 +0900

    Add Custom Route comments

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const router = jsonServer.router('Database.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+// Add Custom Routes
 server.use(router)
+
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
